2b9e1c058facb15a1772cc3ca7a36c170d003b9d
mauricio10am@gmail.com <mauricio10am@gmail.com>
2018-05-11 09:52:39 -0300

    lendo variavel do teclado

--- src/Aula_01.java
+++ src/Aula_01.java
@@ -1,10 +1,20 @@
-    package aulas;
+import java.util.Scanner;   
+
+//package aulas;
         
     public class Aula_01{
     
         public static void main(String[] args){
             
-            System.out.println("Hello word");
+        String nome;
+            
+            Scanner Ler_nome = new Scanner(System.in);
+            
+                System.out.println("Digite seu nome: ");
+                        
+                        nome = Ler_nome.next(); 
+
+                            System.out.printf("Nome: %s ", nome);
         }
         
     }
